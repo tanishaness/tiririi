395e060a9844a2681680c03a8fbc0ae813d6af5e
Tanisha Lalwani <145191259+tanishaness@users.noreply.github.com>
2025-11-17 21:22:22 +0530

    ..

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [a, setA] = useState(20) //read only a, write only setA and their value 20
+
+
+  function changeA() {
+    a ++;
+  }
+
   return (
-    <div className='bg-amber-400'>App</div>
+    <div>
+      <h1>value of a is {a}</h1>
+      <button onClick={changeA}>Click</button>
+    </div>
   )
 }
 
