9b707e896bccaa6aac500f8c3f2315c78461f7f1
Tanisha Lalwani <145191259+tanishaness@users.noreply.github.com>
2025-11-18 03:24:38 +0530

    FORMS

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,19 +1,71 @@
-import React, { useState } from 'react'
-
- 
+/* USE STATE 
+const App = () => {
+  const [a, setA] = useState({a:'tani' , age:22}) 
+  const [arr, setArr] = useState([1,2,3,4,5])
+  
 const arrWork = () => {
-  setUsers((prevUsers) => {
-    return [...prevUsers, 40]
-  })
+  //destructuring assignment
+  const newA = {...a}; //copy object a to newA
+  newA.age = 23; //modify age property
+  setA(newA); //update state with new object
+
+  const newArr = [...arr]; //copy array arr to newArr
+  newArr.push(6);
+  setArr(newArr); //update state with new array
+
+  //OTHER SINTAX
+  // setA(prevA => ({...prevA, age: 23})); //update state with modified age property
+  
+  }
   return (
+
     <div>
-      <h1>Users: {users.join(", ")}</h1>
-      <h1>Value of user is {username}</h1>
-      <h1>value of a is {a}</h1>
-      <button onClick={changeA}>Click</button>
+      <h1>Array elements: {arr.join(', ')}</h1>
+      <h1>value of a is {a.a} and age is {a.age}</h1>
+      <button onClick={arrWork}>Click</button>
     </div>
   )
 }
+*/
+import { useState } from 'react'
+/* 
+const App = () => {
+  //batch update example
+  const [num, setnum] = useState(10)
 
+  const handleClick = () => {
+    setnum(num + 1)
+    setnum(num + 1)
+    setnum(num + 1)
+    console.log(num); //will log the old value of num due to batching
+  }
+ // to do this better
+
+ const handleClick = () => {
+    setnum(prevnum => prevnum + 1)
+    setnum(prevnum => prevnum + 1)
+    setnum(prevnum => prevnum + 1)
+    console.log(num); //will log the old value of num due to batching
+  return (
+    <>
+    <h1>Value of num is {num}</h1>
+    <button onClick={handleClick}>click</button>
+    </>
+    
+  )
+}
+
+export default App
+*/
+
+// INPUT N FORMS
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
 
 export default App
