0f2527662a25da9b774bae228a8cca0a4d63f793
Tanisha Lalwani <145191259+tanishaness@users.noreply.github.com>
2025-11-18 02:02:33 +0530

    02

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,16 +1,10 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [a, setA] = useState(20) 
-  const [username, setUsername] = useState("Tanisha")
-  const [users, setUsers] = useState([10,20,30])
-
-  function changeA() {
-    setA(a + 1);
-    setUsername("Tanisha Kothari");
-    setUsers([...users, a + 1]);
-  }
-
+ 
+const arrWork = () => {
+  setUsers((prevUsers) => {
+    return [...prevUsers, 40]
+  })
   return (
     <div>
       <h1>Users: {users.join(", ")}</h1>
